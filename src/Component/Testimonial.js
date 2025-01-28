60aeb28be0961081a38c5a16897d8740514d3e19
Durgesh Kumar Nishad <durgesh404011@gmail.com>
2025-01-28 16:07:27 +0530

    Update Testimonial.js

--- src/Component/Testimonial.js
+++ src/Component/Testimonial.js
@@ -8,18 +8,18 @@ export default function Testimonial() {
       <div className="flex flex-wrap justify-center gap-8">
         <div className="bg-gray-50 p-6 rounded-lg shadow-lg w-72">
           <p className="text-lg text-gray-600">
-            "Meritto has transformed the way we work. The performance is
-            outstanding!"
+            Meritto has transformed the way we work. The performance is
+            outstanding!
           </p>
           <div className="flex items-center mt-4">
             <img
               src="/path/to/avatar.jpg"
-              alt="John Doe"
+              alt="Alakh Pandey"
               className="w-12 h-12 rounded-full mr-4"
             />
             <div>
-              <h4 className="text-xl font-semibold">John Doe</h4>
-              <p className="text-gray-500">CEO, Example Corp.</p>
+              <h4 className="text-xl font-semibold">Alakh Pandey</h4>
+              <p className="text-gray-500">CEO, PW</p>
             </div>
           </div>
         </div>
