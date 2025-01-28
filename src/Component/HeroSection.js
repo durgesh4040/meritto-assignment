7eb375fbc16225e8913a01b66576138624359671
Durgesh Kumar Nishad <durgesh404011@gmail.com>
2025-01-28 16:31:22 +0530

    Update HeroSection.js

--- src/Component/HeroSection.js
+++ src/Component/HeroSection.js
@@ -1,20 +1,23 @@
-// Hero Component
+
 export default function HeroSection() {
   return (
     <div
       className="bg-cover bg-center h-screen relative"
       style={{ backgroundImage: 'url("./meritto.avif")' }}
     >
+      
       <div className="absolute inset-0 bg-black opacity-50"></div>
+
+    
       <div className="flex items-center justify-center h-full text-center px-4 md:px-8 text-white">
         <div>
           <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold mb-4">
             Welcome to Meritto
           </h1>
-          <h2 className="text-base md:text-lg lg:text-xl mb-6 mt-45">
+          <p className="text-base md:text-lg lg:text-xl mb-6">
             Empowering your journey with cutting-edge solutions.
           </p>
-          <button className="bg-blue-500 text-white py-5 px-6 rounded-lg text-base md:text-xl hover:bg-blue-600 transition">
+          <button className="bg-blue-500 text-white py-3 px-6 rounded-lg text-base md:text-xl hover:bg-blue-600 transition">
             Get Started
           </button>
         </div>
@@ -22,4 +25,5 @@ export default function HeroSection() {
     </div>
   );
 }
+
 
